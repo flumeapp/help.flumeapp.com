2209a93cb4ae979b17e5d9e4b8fc7b88dbab7237
Flume <support@flumeapp.com>
2017-08-05 19:55:02 +0200

    Updates assets/script.js
    Auto commit by GitBook Editor

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,9 @@
 $(window).load(function ()
 {
     var hash = window.location.hash;
-    var tag = $(""+hash+"");
-    $('html, body').animate({scrollTop:$(tag).offset().top}, 2000);
+    if (hash.length)
+    {
+        var tag = $(""+hash+"");
+        $('html, body').animate({scrollTop:$(tag).offset().top}, 2000);
+    }
 });
