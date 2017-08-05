dda8f34ef6c0670837ebec927e1c723d094500ea
Flume <support@flumeapp.com>
2017-08-05 20:07:58 +0200

    Track 1 files into repository.
    - modified assets/script.js
    
    Auto commit by GitBook Editor

--- assets/script.js
+++ assets/script.js
@@ -7,14 +7,3 @@ setTimeout(function()
 		location.hash = hash;
 	}
 }, 1000);
-
-// $(window).on('load', function()
-// {
-//     var hash = window.location.hash;
-//     if (hash.length)
-//     {
-//     	location.hash = hash;
-//         var tag = $(""+hash+"");
-//         $('html, body').animate({scrollTop:$(tag).offset().top}, 2000);
-//     }
-// });
